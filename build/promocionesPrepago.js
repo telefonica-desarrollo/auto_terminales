9828ab3534e47e216fb90500524466e52d994492
Jesus Arath <jesus.flores.p19@gmail.com>
2021-11-23 14:58:45 -0600

    Documento de promocioesPrepago

---
+++ build/promocionesPrepago.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.promocionesPrepago = void 0;
+let fechaInicio = "01/11/2021";
+fechaInicio = new Date(fechaInicio).getTime();
+let fechaFinal = "17/11/2021";
+fechaFinal = new Date(fechaFinal).getTime();
+let fechaActual = new Date().getTime();
+function promocionesPrepago() {
+}
+exports.promocionesPrepago = promocionesPrepago;
---
+++ src/promocionesPrepago.ts
@@ -0,0 +1,15 @@
+import * as fs from "fs"
+import * as EXcelJS from "exceljs"
+import path from "path"
+
+let fechaInicio: any = "01/11/2021"
+fechaInicio = new Date(fechaInicio).getTime();
+
+let fechaFinal: any = "17/11/2021"
+fechaFinal = new Date(fechaFinal).getTime();
+
+let fechaActual = new Date().getTime()
+
+export function promocionesPrepago(){
+
+}
