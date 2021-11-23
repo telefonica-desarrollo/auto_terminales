3fabe7bfa5252058bf903459fe1ae4d6b680be37
Jesus Arath <jesus.flores.p19@gmail.com>
2021-11-23 15:25:20 -0600

    Algoritmo basico entre fechas

--- src/promocionesPrepago.ts
+++ src/promocionesPrepago.ts
@@ -2,14 +2,34 @@ import * as fs from "fs"
 import * as EXcelJS from "exceljs"
 import path from "path"
 
-let fechaInicio: any = "01/11/2021"
-fechaInicio = new Date(fechaInicio).getTime();
-
-let fechaFinal: any = "17/11/2021"
-fechaFinal = new Date(fechaFinal).getTime();
-
 let fechaActual = new Date().getTime()
 
-export function promocionesPrepago(){
+let data: any = []
+let promociones: any [];
+let i = 0
 
+export function promocionesPrepago(){
+    let woorkbook = new EXcelJS.Workbook();
+    const documento = path.join(__dirname + "/docs/Terminales.xlsx")
+    
+    woorkbook.xlsx.readFile(documento).then(function() {
+        var woorksheet = woorkbook.worksheets[0]
+        woorksheet.eachRow((row, rowNumber)=>{
+        
+            let fechaInicio: any = row.getCell(13).value
+            fechaInicio = new Date(fechaInicio).getTime();
+            
+            let fechaFinal: any = row.getCell(14).value
+            fechaFinal = new Date(fechaFinal).getTime();
+            
+            if(fechaInicio < fechaActual && fechaActual < fechaInicio){
+                if(i == 0) i=1;
+                if(i > 0 ) i++
+            }
+            
+            // data = []
+            // data.SKU = row.getCell(1).value
+        })
+    })
+    
 }
