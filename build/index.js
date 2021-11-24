2c530e1b37212d0cdf622c5b00e6508551b9985a
Jesus Arath <jesus.flores.p19@gmail.com>
2021-11-23 18:43:48 -0600

    Cambios del index

--- build/index.js
+++ build/index.js
@@ -25,30 +25,35 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const EXcelJS = __importStar(require("exceljs"));
 const fs = __importStar(require("fs"));
 const path_1 = __importDefault(require("path"));
+const promocionesPg = __importStar(require("./promocionesPrepago"));
 let data = [];
 let tiendas = JSON.parse(fs.readFileSync("./data/tiendas.txt", "utf8"));
 let woorkbook = new EXcelJS.Workbook();
 const documento = path_1.default.join(__dirname + "/docs/Terminales.xlsx");
-// woorkbook.xlsx.readFile(documento).then(function() {
-//     var woorksheet = woorkbook.worksheets[0]
-//     woorksheet.eachRow((row, rowNumber)=>{
-//         data = []
-//         data.SKU = row.getCell(1).value
-//         data.DENOMINACION = row.getCell(2).value
-//         data.ALMACEN = row.getCell(3).value
-//         data.CANTIDAD = row.getCell(5).value
-//         data.MARCA = row.getCell(6).result
-//         data.PRECIO = row.getCell(7).result
-//         data.IDPDV = row.getCell(9).result
-//         tiendas.forEach((tienda: any)=>{
-//             if(tienda.IDPDV == data.IDPDV){
-//               if(!tienda.TERMINALES) tienda.TERMINALES = []
-//               tienda.TERMINALES.push(data)
-//             }
-//         })
-//     })
-//     for(let terminal of tiendas[0].TERMINALES){
-//       if(terminal.CANTIDAD > 10 ) console.log(terminal)
-//     }
-// })
+const promocionesPrepago = promocionesPg.promocionesPrepago();
+const promocionesPospago = promocionesPg.promocionesPrepago();
+woorkbook.xlsx.readFile(documento).then(function () {
+    var woorksheet = woorkbook.worksheets[0];
+    woorksheet.eachRow((row, rowNumber) => {
+        data = [];
+        data.SKU = row.getCell(1).value;
+        data.DENOMINACION = row.getCell(2).value;
+        data.ALMACEN = row.getCell(3).value;
+        data.CANTIDAD = row.getCell(5).value;
+        data.MARCA = row.getCell(6).result;
+        data.PRECIO = row.getCell(7).result;
+        data.IDPDV = row.getCell(9).result;
+        tiendas.forEach((tienda) => {
+            if (tienda.IDPDV == data.IDPDV) {
+                if (!tienda.TERMINALES)
+                    tienda.TERMINALES = [];
+                tienda.TERMINALES.push(data);
+            }
+        });
+    });
+    for (let terminal of tiendas[0].TERMINALES) {
+        if (terminal.CANTIDAD > 10)
+            console.log(terminal);
+    }
+});
 ///////////////////////////////////////////////////////////////////// Promociones Prepago
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as EXcelJS from "exceljs"
 import * as fs from "fs"
 import path from "path"
+import * as promocionesPg from "./promocionesPrepago"
 
 let data: any = []
 let tiendas = JSON.parse(fs.readFileSync("./data/tiendas.txt", "utf8"));
@@ -8,32 +9,35 @@ let tiendas = JSON.parse(fs.readFileSync("./data/tiendas.txt", "utf8"));
 let woorkbook = new EXcelJS.Workbook();
 const documento = path.join(__dirname + "/docs/Terminales.xlsx")
 
-// woorkbook.xlsx.readFile(documento).then(function() {
-//     var woorksheet = woorkbook.worksheets[0]
-//     woorksheet.eachRow((row, rowNumber)=>{
-    
-//         data = []
-//         data.SKU = row.getCell(1).value
-//         data.DENOMINACION = row.getCell(2).value
-//         data.ALMACEN = row.getCell(3).value
-//         data.CANTIDAD = row.getCell(5).value
-//         data.MARCA = row.getCell(6).result
-//         data.PRECIO = row.getCell(7).result
-//         data.IDPDV = row.getCell(9).result
-        
-//         tiendas.forEach((tienda: any)=>{
-//             if(tienda.IDPDV == data.IDPDV){
-//               if(!tienda.TERMINALES) tienda.TERMINALES = []
-//               tienda.TERMINALES.push(data)
-//             }
-//         })
-//     })
+const promocionesPrepago =  promocionesPg.promocionesPrepago()
+const promocionesPospago =  promocionesPg.promocionesPrepago()
 
-//     for(let terminal of tiendas[0].TERMINALES){
-//       if(terminal.CANTIDAD > 10 ) console.log(terminal)
-//     }
+woorkbook.xlsx.readFile(documento).then(function() {
+    var woorksheet = woorkbook.worksheets[0]
+    woorksheet.eachRow((row, rowNumber)=>{
     
-// })
+        data = []
+        data.SKU = row.getCell(1).value
+        data.DENOMINACION = row.getCell(2).value
+        data.ALMACEN = row.getCell(3).value
+        data.CANTIDAD = row.getCell(5).value
+        data.MARCA = row.getCell(6).result
+        data.PRECIO = row.getCell(7).result
+        data.IDPDV = row.getCell(9).result
+        
+        tiendas.forEach((tienda: any)=>{
+            if(tienda.IDPDV == data.IDPDV){
+              if(!tienda.TERMINALES) tienda.TERMINALES = []
+              tienda.TERMINALES.push(data)
+            }
+        })
+    })
+
+    for(let terminal of tiendas[0].TERMINALES){
+      if(terminal.CANTIDAD > 10 ) console.log(terminal)
+    }
+    
+})
 
 ///////////////////////////////////////////////////////////////////// Promociones Prepago
 
