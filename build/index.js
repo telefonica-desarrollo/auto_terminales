2fb5e23e723e6fdddf6ccc82d26baba76fc23e43
Jesus Arath <jesus.flores.p19@gmail.com>
2021-11-29 11:01:04 -0600

    Cambios en index

--- build/index.js
+++ build/index.js
@@ -26,29 +26,70 @@ const EXcelJS = __importStar(require("exceljs"));
 const fs = __importStar(require("fs"));
 const path_1 = __importDefault(require("path"));
 const promocionesPg = __importStar(require("./controller/promocionesPrepago"));
-let data = [];
+const promocionesPp = __importStar(require("./controller/promocionesPospago"));
+const firestore = __importStar(require("./controller/Firestore"));
+let data = {};
+let contador = 0;
 let tiendas = JSON.parse(fs.readFileSync("./data/tiendas.txt", "utf8"));
 let woorkbook = new EXcelJS.Workbook();
 const documento = path_1.default.join(__dirname + "/docs/Terminales.xlsx");
-const promocionesPrepago = promocionesPg.promocionesPrepago();
-const promocionesPospago = promocionesPg.promocionesPrepago();
-woorkbook.xlsx.readFile(documento).then(function () {
-    var woorksheet = woorkbook.worksheets[0];
-    woorksheet.eachRow((row, rowNumber) => {
-        data = [];
-        data.SKU = row.getCell(1).value;
-        data.DENOMINACION = row.getCell(2).value;
-        data.ALMACEN = row.getCell(3).value;
-        data.CANTIDAD = row.getCell(5).value;
-        data.MARCA = row.getCell(6).result;
-        data.PRECIO = row.getCell(7).result;
-        data.IDPDV = row.getCell(9).result;
-        tiendas.forEach((tienda) => {
-            if (tienda.IDPDV == data.IDPDV) {
-                if (!tienda.TERMINALES)
-                    tienda.TERMINALES = [];
-                tienda.TERMINALES.push(data);
-            }
+let promocionesPrepago, promocionesPospago;
+///////////////////////////////// Promociones de Prepago
+promocionesPg.promocionesPrepago().then((data) => {
+    promocionesPrepago = data;
+    // console.log(promocionesPrepago.length);
+}).then(() => {
+    ////////////////////////////// Promociones de Pospago
+    promocionesPp.promocionesPospago().then((data) => {
+        promocionesPospago = data;
+        // console.log(promocionesPospago.length);
+    }).then(() => {
+        ///////////////////////////Excel de Terminales
+        woorkbook.xlsx.readFile(documento).then(function () {
+            var woorksheet = woorkbook.worksheets[0];
+            woorksheet.eachRow((row, rowNumber) => {
+                let prom_pre = false, prom_pos = false;
+                data = {};
+                data.SKU = row.getCell(1).value;
+                data.DENOMINACION = row.getCell(2).value;
+                data.ALMACEN = row.getCell(3).value;
+                data.CANTIDAD = row.getCell(5).value;
+                data.MARCA = row.getCell(6).result;
+                data.PRECIO = row.getCell(7).result;
+                const IDPDV = row.getCell(9).result;
+                tiendas.forEach((tienda) => {
+                    if (tienda.IDPDV == IDPDV) {
+                        //PROCIONES PREPAGO POR INVENTARIO DE CADA PUNTO DE VENTA
+                        promocionesPrepago.forEach((promocion) => {
+                            if (promocion.SKU == data.SKU) {
+                                if (!tienda.PROMOCIONES_PREPAGO)
+                                    tienda.PROMOCIONES_PREPAGO = [];
+                                data.PVP = promocion.PVP;
+                                data.PORCENTAJE = promocion.PORCENTAJE;
+                                contador++;
+                                tienda.PROMOCIONES_PREPAGO.push(data);
+                            }
+                        });
+                        //PROCIONES POSPAGO POR INVENTARIO DE CADA PUNTO DE VENTA
+                        promocionesPospago.forEach((promocion) => {
+                            if (promocion.SKU == data.SKU) {
+                                if (!tienda.PROMOCIONES_POSPAGO)
+                                    tienda.PROMOCIONES_POSPAGO = [];
+                                data.PVP = promocion.PVP;
+                                data.PORCENTAJE = promocion.PORCENTAJE;
+                                contador++;
+                                tienda.PROMOCIONES_POSPAGO.push(data);
+                            }
+                        });
+                    }
+                });
+            });
+            console.log(contador);
+            fs.writeFile("./tienda.txt", JSON.stringify(tiendas[0]), () => {
+                console.log("hecho panas");
+            });
+            ///SUBIR DATA A FIRESTORE  
+            firestore.subirData(tiendas);
         });
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -2,38 +2,83 @@ import * as EXcelJS from "exceljs"
 import * as fs from "fs"
 import path from "path"
 import * as promocionesPg from "./controller/promocionesPrepago"
+import * as promocionesPp from "./controller/promocionesPospago"
+import * as firestore from "./controller/Firestore"
 
-let data: any = []
+let data: any = {}
+let contador = 0
 let tiendas = JSON.parse(fs.readFileSync("./data/tiendas.txt", "utf8"));
 
 let woorkbook = new EXcelJS.Workbook();
 const documento = path.join(__dirname + "/docs/Terminales.xlsx")
 
-const promocionesPrepago =  promocionesPg.promocionesPrepago()
-const promocionesPospago =  promocionesPg.promocionesPrepago()
+let promocionesPrepago: any, promocionesPospago: any;
+///////////////////////////////// Promociones de Prepago
+promocionesPg.promocionesPrepago().then( (data: any)=>{
+    promocionesPrepago = data
+    // console.log(promocionesPrepago.length);
+}).then(()=> {
+    ////////////////////////////// Promociones de Pospago
+    promocionesPp.promocionesPospago().then( (data: any)=> {
+        promocionesPospago = data;
+        // console.log(promocionesPospago.length);
+    }).then(()=>{
+        ///////////////////////////Excel de Terminales
+        woorkbook.xlsx.readFile(documento).then(function() {
+            var woorksheet = woorkbook.worksheets[0]
+            woorksheet.eachRow((row, rowNumber)=>{
+            
+                let prom_pre = false, prom_pos = false;
+                data = {}
 
-woorkbook.xlsx.readFile(documento).then(function() {
-    var woorksheet = woorkbook.worksheets[0]
-    woorksheet.eachRow((row, rowNumber)=>{
-    
-        data = []
-        data.SKU = row.getCell(1).value
-        data.DENOMINACION = row.getCell(2).value
-        data.ALMACEN = row.getCell(3).value
-        data.CANTIDAD = row.getCell(5).value
-        data.MARCA = row.getCell(6).result
-        data.PRECIO = row.getCell(7).result
-        data.IDPDV = row.getCell(9).result
-        
-        tiendas.forEach((tienda: any)=>{
-            if(tienda.IDPDV == data.IDPDV){
-              if(!tienda.TERMINALES) tienda.TERMINALES = []
-              tienda.TERMINALES.push(data)
-            }
+
+                data.SKU = row.getCell(1).value
+                data.DENOMINACION = row.getCell(2).value
+                data.ALMACEN = row.getCell(3).value
+                data.CANTIDAD = row.getCell(5).value
+                data.MARCA = row.getCell(6).result
+                data.PRECIO = row.getCell(7).result
+                const IDPDV = row.getCell(9).result
+                
+                tiendas.forEach((tienda: any)=>{
+                    if(tienda.IDPDV == IDPDV){
+                        //PROCIONES PREPAGO POR INVENTARIO DE CADA PUNTO DE VENTA
+                        promocionesPrepago.forEach((promocion: any) => {
+                            if(promocion.SKU == data.SKU){
+                                if(!tienda.PROMOCIONES_PREPAGO) tienda.PROMOCIONES_PREPAGO = []
+                                data.PVP = promocion.PVP;
+                                data.PORCENTAJE = promocion.PORCENTAJE;
+                                contador++
+
+                                tienda.PROMOCIONES_PREPAGO.push(data)
+                            }
+                        })
+                        //PROCIONES POSPAGO POR INVENTARIO DE CADA PUNTO DE VENTA
+                        promocionesPospago.forEach((promocion: any) => {
+                            if(promocion.SKU == data.SKU){
+                                if(!tienda.PROMOCIONES_POSPAGO) tienda.PROMOCIONES_POSPAGO = []
+                                data.PVP = promocion.PVP;
+                                data.PORCENTAJE = promocion.PORCENTAJE
+                                contador++
+
+                                tienda.PROMOCIONES_POSPAGO.push(data)
+                            }
+                        })
+                    }
+
+                })
+            })
+            console.log(contador);
+            fs.writeFile("./tienda.txt", JSON.stringify(tiendas[0]), ()=> {
+                console.log("hecho panas");
+            })
+            ///SUBIR DATA A FIRESTORE  
+            firestore.subirData(tiendas)
         })
     })
-    
 })
+
+
 
 
 
