f60e582aa3f970ab636c82e406ccb5cfc2081f94
Jesus Arath <jesus.flores.p19@gmail.com>
2022-01-01 23:45:48 -0600

    Funciones del controllador

--- build/controller/index.controller.js
+++ build/controller/index.controller.js
@@ -1,9 +1,50 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.indexController = void 0;
+const database_1 = __importDefault(require("../database"));
 class IndexController {
+    constructor() {
+        this.tiendas = [];
+        database_1.default;
+    }
     obtenerTiendas(req, res) {
-        res.send("Hola mundo desde el controller");
+        return __awaiter(this, void 0, void 0, function* () {
+            yield database_1.default.query("Select * from TIENDAS", (err, result, fields) => {
+                res.json(result);
+            });
+        });
+    }
+    validarUsuario(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const data = req.body;
+            console.log(data);
+            const sql = "Select * from USUARIOS where Usuario = ? && Password = ?";
+            yield database_1.default.query(sql, [data.usuario, data.password], (err, result) => {
+                try {
+                    if (err)
+                        throw "Peticion no valida";
+                    if (result.length > 0)
+                        res.json(true);
+                    else
+                        res.json(false);
+                }
+                catch (error) {
+                    console.log(error);
+                }
+            });
+        });
     }
 }
 exports.indexController = new IndexController();
--- src/controller/index.controller.ts
+++ src/controller/index.controller.ts
@@ -1,9 +1,36 @@
 import { Request, Response } from 'express';
+import con from "../database" 
 
 class IndexController{
-    obtenerTiendas(req: Request, res: Response){
-        res.send("Hola mundo desde el controller")
+    tiendas: any = []; 
+    constructor(){
+        con;
     }
+
+    async obtenerTiendas(req: Request, res: Response){
+        await con.query("Select * from TIENDAS", (err, result, fields) => {
+            res.json(result)
+        });
+    }
+    async validarUsuario(req: Request, res: Response){
+        const data: any= req.body;
+        console.log(data);
+        
+        const sql = "Select * from USUARIOS where Usuario = ? && Password = ?"
+        await con.query(sql , [data.usuario, data.password] , (err, result) => {
+            try {
+                if(err) throw "Peticion no valida";
+
+                if(result.length > 0) res.json(true)
+                else res.json(false)     
+
+            } catch (error) {
+                console.log(error);
+            }
+        })
+
+    }
+
 }
 
 export const indexController = new IndexController();
